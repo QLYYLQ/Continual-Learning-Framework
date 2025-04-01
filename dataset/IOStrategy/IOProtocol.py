9198622182360d7bec813d2c89ec8868e0bf62fc
QLYYLQ <1965658150@qq.com>
2025-04-01 17:16:53 +0800

    finish IOMeta

---
+++ dataset/IOStrategy/IOProtocol.py
@@ -0,0 +1,39 @@
+from typing import List, Protocol, runtime_checkable, Union
+from abc import ABC, abstractmethod
+from os import PathLike
+import torch
+
+
+class LoadProtocol(Protocol):
+
+    def load(self, file_name: Union[str, PathLike[str]]) -> List[List[int]]:
+        ...
+
+
+class WriteProtocol(Protocol):
+    def write(self, file_name: Union[str, PathLike[str]]) -> None:
+        ...
+
+
+@runtime_checkable
+class IOProtocol(LoadProtocol, WriteProtocol, Protocol):
+    """
+    Canonical IO strategy must have load and write methods.
+    """
+    pass
+
+
+class IOMeta(type):
+    """
+    auto registry the IO class
+    """
+    _registry = {}
+
+    def __init__(cls, name, bases, attrs):
+        super().__init__(name, bases, attrs)
+        if hasattr(cls, "suffixes"):
+            for suffix in cls.suffixes:
+                if suffix in cls._registry:
+                    existing = cls._registry[suffix].__name__
+                    raise ValueError(f".{suffix} is already registered by {existing}")
+                IOMeta._registry[suffix] = cls
--- dataset/__init__.py
+++ dataset/__init__.py
@@ -5,4 +5,4 @@ from .dataloader import Dataloader
 from .utils.BaseDataset import BaseDataset
 from .base_dataset import BaseSplit, BaseIncrement
 from .utils.filter_images import filter_images, save_list_from_filter
-from .LoadStrategy.BaseLoader import LoadProtocol
+from .IOStrategy.BaseLoader import LoadProtocol
