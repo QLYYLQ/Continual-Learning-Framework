f22912337328e55c5232a80b207b3567c427ddcb
QLYYLQ <1965658150@qq.com>
2025-04-08 00:23:54 +0800

    fix IOProtocol with mypy

--- dataset/IOStrategy/IOProtocol.py
+++ dataset/IOStrategy/IOProtocol.py
@@ -1,10 +1,18 @@
-from typing import List, Protocol, runtime_checkable, Union, Any, ClassVar, Final
-from abc import ABC, abstractmethod, ABCMeta
+from typing import List, Protocol, runtime_checkable, Union, Any, Dict, Iterable, Optional, TypedDict, TypeVar, Type
 from os import PathLike
 from weakref import WeakSet
 
-_Registry: dict[str, dict[str, object]] = {}
 
+class _T_ModalityRegistry(TypedDict):
+    BaseIO: Type[Any]
+    base_suffixes: List[str]
+    Custom: Dict[str, Type[Any]]
+
+
+
+_T_Registry = Dict[str, _T_ModalityRegistry]
+
+_Registry: _T_Registry = dict()
 
 class LoadProtocol(Protocol):
 
@@ -31,22 +39,29 @@ class IOMeta(type):
     Auto Register for subclass of BaseIO, manually register for subclass of IOProtocol
     This class is used inside Framework
     """
+
     _io_invalidation_counter = 0
 
     def __new__(mcls, name, bases, namespace):
+        # check the basic attribute
         modality = getattr(mcls, "modality", None)
         is_base = getattr(mcls, "is_base", None)
         assert modality is not None and is_base is not None, "You should set modality and is_base"
+
+        # create class
         cls = super().__new__(mcls, name, bases, namespace)
+
+        # set attribute for class
         setattr(cls, "modality", modality)
         setattr(cls, "is_base", is_base)
         suffixes_list = getattr(cls, 'suffixes', [])
-        if not suffixes_list and not is_base:
+        suffixes_set = _check_suffixes(suffixes_list)
+        if not suffixes_set and not is_base:
             raise ValueError(f"{name} must have suffixes attribute")
         # registering with different modality, first check exits modality
         if modality not in _Registry:
             _Registry[modality] = dict()
-        mcls._meta_register(cls, mcls.is_base, _Registry[modality], suffixes_list)
+        mcls._meta_register(cls, is_base, _Registry[modality], suffixes_set)
         mcls.is_base = False
         # add base implementation
         cls._io_registry = WeakSet()
@@ -57,7 +72,7 @@ class IOMeta(type):
         cls._io_invalidation_cache_version = IOMeta._io_invalidation_counter
         return cls
 
-    def register(cls, subclass, suffixes: List[str] = None):
+    def register(cls, subclass, suffixes: Optional[Iterable[str]] = None):
         """
         you can register a subclass with this method, without inheriting from BaseIO.
         You can also manually set suffixes
@@ -71,21 +86,23 @@ class IOMeta(type):
             # register a subclass with this method, only open .jpg suffix, not .png or other
             BaseIO.register(MyIO, suffixes=['.jpg'])
         """
-        if not issubclass(subclass, (type, IOProtocol)):
-            raise TypeError("Can only register classes with load and write methods, subclass not have this method, "
-                            "or not a class")
+        if not isinstance(subclass, type):
+            raise TypeError("you can only register a class")
+        if not issubclass(subclass, IOProtocol):
+            raise TypeError("you can only register a subclass of IOProtocol(has load and write method)")
         if suffixes is None:
             suffixes = getattr(subclass, "suffixes", None)
         assert suffixes is not None, (f"if you want to register a subclass of {cls.__qualname__}, you must define "
                                       f"suffixes")
+        suffixes = _check_suffixes(suffixes)
         # Already a subclass of cls, not need for registry
         if issubclass(subclass, cls):
             return subclass
         if issubclass(cls, subclass):
             raise RuntimeError(f"circular reference: {cls} is subclass of {subclass}")
-        cls._io_registry.add(subclass)
+        cls._io_registry.add(subclass)  # type: ignore
         # when you manually register class, that won't be base class
-        cls._meta_register(subclass, False, _Registry[cls.modality], suffixes)
+        cls._meta_register(subclass, False, _Registry[cls.modality], suffixes)  # type: ignore
         IOMeta._io_invalidation_counter += 1
         return subclass
 
@@ -130,7 +147,10 @@ class IOMeta(type):
         return False
 
     @staticmethod
-    def _meta_register(cls, is_base, registry: dict, suffixes_list: List[str], ):
+    def _meta_register(cls, is_base, registry: dict, suffixes_list: Iterable[str], ):
+        """
+        auto registry
+        """
         if is_base:
             registry['base_suffixes'] = suffixes_list
             registry["BaseIO"] = cls
@@ -143,7 +163,7 @@ class IOMeta(type):
                     registry[suffix] = cls
 
 
-def create_io_registry(modality: str, is_base: bool, cls_name: str = None) -> type:
+def create_io_registry(modality: str, is_base: bool = True, cls_name: Optional[str] = None) -> type:
     if cls_name is None:
         cls_name = f"{modality.capitalize()}IOMeta"
     attrs = {
@@ -156,11 +176,23 @@ def create_io_registry(modality: str, is_base: bool, cls_name: str = None) -> ty
     return new_meta
 
 
+def _check_suffixes(suffixes: Iterable[str]) -> set:
+    return_suffixes = []
+    for suffix in suffixes:
+        # The suffix may have uppercase letters, so don't make any adjustment here
+        if suffix[0] == ".":
+            # fix possible clerical errors by users
+            return_suffixes.append(suffix.lstrip("."))
+        else:
+            return_suffixes.append(suffix)
+    return set(return_suffixes)
+
+
 if __name__ == "__main__":
     ImageIOMeta = create_io_registry("image", True)
 
 
-    class BaseIO(metaclass=ImageIOMeta):
+    class BaseIO(metaclass=ImageIOMeta):  # type: ignore
         """
         You must add all possible suffixes to the list, and files with these suffixes should be able to be read and
         written by the methods you set
@@ -180,7 +212,7 @@ if __name__ == "__main__":
             raise NotImplementedError
 
 
-    class TryClass1(ImageIOMeta):
+    class TryClass1(ImageIOMeta):  # type: ignore
         suffixes = ["try1"]
 
         def load(self, **kwargs):
@@ -227,9 +259,9 @@ if __name__ == "__main__":
 
     # print(dir(BaseIO))
     # BaseIO.register(TryClass1)
-    BaseIO.register(TryClass2)
+    BaseIO.register(TryClass2)  # type: ignore
     # print(issubclass(TryClass3, IOProtocol))
-    BaseIO.register(TryClass4)
+    BaseIO.register(TryClass4)  # type: ignore
     print(_Registry)
     # BaseIO.register(BaseIO2)
     # BaseIO.register(TryClass3)
