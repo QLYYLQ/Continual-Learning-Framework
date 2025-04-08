cda2bbfbeff38bdbb234867495c48a3bfa66117a
QLYYLQ <1965658150@qq.com>
2025-04-08 10:55:05 +0800

    fix __init__.py, not using relative names

--- dataset/IOStrategy/__init__.py
+++ dataset/IOStrategy/__init__.py
@@ -1,2 +1,9 @@
-from .IOProtocol import IOProtocol, BaseIO, IOMeta
-IORegister = BaseIO.registry
+from dataset.IOStrategy.IOProtocol import IOProtocol, create_io_registry, IOMeta
+from dataset.IOStrategy.IOProtocol import _Registry as IORegistry
+
+__all__=[
+    "create_io_registry",
+    "IOMeta",
+    "IOProtocol",
+    "IORegistry",
+]
